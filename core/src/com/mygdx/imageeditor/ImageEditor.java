7cbc9911147c9e752d8a4558b23c9dac98d11a1b
Spence <120441242+5pencer5mith@users.noreply.github.com>
2024-03-28 11:18:59 -0600

    Outputs green square on red background
    Created a rectangle2D class

--- core/src/com/mygdx/imageeditor/ImageEditor.java
+++ core/src/com/mygdx/imageeditor/ImageEditor.java
@@ -1,25 +1,33 @@
 package com.mygdx.imageeditor;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ImageEditor extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Pixmap rectangleMap;
+	Rectangle2D rectangle;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		rectangle = new Rectangle2D(new Vector2(200, 350), new Vector2(100, 100), Color.CHARTREUSE);
+		
+		
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(rectangle.RecTexture, rectangle.Position.x, rectangle.Position.y);
 		batch.end();
 	}
 	
--- desktop/src/com/mygdx/imageeditor/DesktopLauncher.java
+++ desktop/src/com/mygdx/imageeditor/DesktopLauncher.java
@@ -9,7 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("ImageEditor");
+		config.setTitle("Image Editor");
+		System.out.println("Project made by: Spencer Smith and (?)");
 		new Lwjgl3Application(new ImageEditor(), config);
 	}
 }
